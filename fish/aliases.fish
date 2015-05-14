ac5c6b18f5ff0c9ed2b9ed28a5572e8f4be939dd
Iain Cambridge <i.cambridge@foodpanda.com>
2015-05-14 21:17:30 +0200

    Add aliases fish file

---
+++ fish/aliases.fish
@@ -0,0 +1,7 @@
+alias ..="cd ../"
+alias ...="cd ../../"
+alias phpsec="bin/phpspec"
+alias phpspec="bin/phpspec"
+alias startvm="cd ~/code/foodpanda/vagrant-dev; vagrant up"
+alias sshvm="cd ~/code/foodpanda/vagrant-dev; vagrant ssh"
+
