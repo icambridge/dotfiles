2baa72912b2781445addef428045e22d2d10094e
Iain Cambridge <i.cambridge@foodpanda.com>
2015-05-27 08:28:43 +0200

    Add env.fish and ocaml utop

---
+++ fish/env.fish
@@ -0,0 +1,7 @@
+set -gx PATH /Users/icambridge/.opam/system/bin $PATH
+
+set -gx CAML_LD_LIBRARY_PATH "/Users/icambridge/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"
+set -gx OPAMUTF8MSGS "1"
+set -gx MANPATH ":/Users/icambridge/.opam/system/man"
+set -gx PERL5LIB "/Users/icambridge/.opam/system/lib/perl5"
+set -gx OCAML_TOPLEVEL_PATH "/Users/icambridge/.opam/system/lib/toplevel"
