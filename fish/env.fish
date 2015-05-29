5ac348be6826183d18b9bd48b789c562d0cfea06
Iain Cambridge <i.cambridge@foodpanda.com>
2015-05-29 08:39:25 +0200

    Add nimble bin to PATH

--- fish/env.fish
+++ fish/env.fish
@@ -1,3 +1,4 @@
+# Ocaml
 set -gx PATH /Users/icambridge/.opam/system/bin $PATH
 
 set -gx CAML_LD_LIBRARY_PATH "/Users/icambridge/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"
@@ -5,3 +6,7 @@ set -gx OPAMUTF8MSGS "1"
 set -gx MANPATH ":/Users/icambridge/.opam/system/man"
 set -gx PERL5LIB "/Users/icambridge/.opam/system/lib/perl5"
 set -gx OCAML_TOPLEVEL_PATH "/Users/icambridge/.opam/system/lib/toplevel"
+
+# Nim
+
+set -gx PATH /Users/icambridge/.nimble/bin $PATH
